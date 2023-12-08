771bd349c921833a857bdee6e75cde59d8474bf5
Bhautik Navdariya <navadariyabhautikm@gmail.com>
2023-12-08 06:46:09 -0500

    add-forget pass

--- registration-app/src/app/app-routing.module.ts
+++ registration-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
 
 const routes: Routes = [
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'forgot-pass', component:ForgotPassComponent}
 ];
 
 @NgModule({
--- registration-app/src/app/app.module.ts
+++ registration-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ForgotPassComponent
   ],
   imports: [
     BrowserModule,
