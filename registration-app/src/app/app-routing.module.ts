0978a1b7375e4194ea6df3b3b662493df7c5c5a4
Bhautik Navdariya <navadariyabhautikm@gmail.com>
2023-12-09 06:15:58 -0500

    my fault

--- registration-app/src/app/app-routing.module.ts
+++ registration-app/src/app/app-routing.module.ts
@@ -9,9 +9,11 @@ import { AttendenceComponent } from './attendence/attendence.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { InboxComponent } from './inbox/inbox.component';
 import { AttendenceSheetComponent } from './attendence-sheet/attendence-sheet.component';
+import { AppComponent } from './app.component';
  
 
 const routes: Routes = [
+  {path:'', component:AppComponent},
   {path:'login', component:LoginComponent},
   {path:'forgot-pass', component:ForgotPassComponent},
   {path:'admin', component:AdminComponent},
