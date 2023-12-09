b344394dd559534ecf74587e0a2e23271ca94ba0
ronit2327 <22010101096@darshan.ac.in>
2023-12-09 14:38:17 +0530

    leave-form update2

--- registration-app/src/app/leave-form/leave-form.component.ts
+++ registration-app/src/app/leave-form/leave-form.component.ts
@@ -1,31 +1,14 @@
+// leave-form.component.ts
+
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-leave-form',
   templateUrl: './leave-form.component.html',
-  styleUrl: './leave-form.component.css'
+  styleUrls: ['./leave-form.component.css']
 })
 export class LeaveFormComponent {
-  leaveForm: FormGroup;
-  result: string = '';
+  leaveApplication: any = {}; // Initialize with empty object
 
-  constructor(private fb: FormBuilder) {
-    this.leaveForm = this.fb.group({
-      reason: ['', Validators.required],
-      days: [1, [Validators.required, Validators.min(1)]],
-      startDate: ['', Validators.required],
-      endDate: ['', Validators.required],
-    });
-  }
-
-  calculateLeave() {
-    const daysTaken = this.leaveForm.value.days;
-
-    if (daysTaken <= 5) {
-      this.result = 'Paid Leave';
-    } else {
-      this.result = 'Unpaid Leave';
-    }
-  }
+  // You can add additional logic here if needed
 }
